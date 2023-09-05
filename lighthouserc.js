39b3668c266e09d175a04fabaccb004f780c6a09
Seong Gi Dong <sgd0947@gmail.com>
2023-09-05 14:35:31 +0900

    feat: lhci 변경

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,42 +4,46 @@ module.exports = {
       startServerCommand: 'npm run start',
       startServerReadyPattern: 'ready on',
       url: ['http://localhost:3000'],
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+        skipAudits: ['uses-http2'],
+        chromeFlags: '--no-sandbox',
+        extraHeaders: JSON.stringify({
+          Cookie: 'customCookie=1;foo=bar',
+        }),
+      },
     },
     upload: {
       target: 'temporary-public-storage',
     },
     assert: {
       assertions: {
-        'categories:pwa': 'off',
         'themed-omnibox': 'off',
-        'tap-targets': 'off',
         'splash-screen': 'off',
         'service-worker': 'off',
-        'meta-viewport': 'off',
         'content-width': 'off',
         'csp-xss': 'off',
-        'font-size': 'off',
         'installable-manifest': 'off',
-        'valid-source-maps': 'off',
         'maskable-icon': 'off',
-        'total-byte-weight': 'off',
-        'unminified-javascript': 'off',
-        'unused-javascript': 'off',
-        'apple-touch-icon': 'off',
-        'document-title': 'off',
-        'errors-in-console': 'off',
-        'meta-description': 'off',
-        'html-has-lang': 'off',
-        'robots-txt': 'off',
         'bf-cache': 'off',
         'unused-css-rules': 'off',
-        viewport: 'off',
+
         // performance 카테고리 점수가 70점 미만이면 warning
         'categories:performance': ['warn', { minScore: 0.7 }],
         // accessibility 가 70점 미만이면 error
         'categories:accessibility': ['error', { minScore: 0.7 }],
         // seo 가 70점 미만이면 error
         'categories:seo': ['error', { minScore: 0.7 }],
+        // best-practices 가 70점 미만이면 error
+        'categories:best-practices': [
+          'error',
+          { minScore: 0.7, aggregationMethod: 'pessimistic' },
+        ],
       },
       preset: 'lighthouse:recommended',
     },
