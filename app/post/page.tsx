b4f0b0305d9c13bcc1c68f99c7b70e9764567b8a
Seong Gi Dong <sgd0947@gmail.com>
2023-09-08 16:34:47 +0900

    feat(layout): metadata에 template 설정

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,14 @@
 import { dehydrate } from '@tanstack/query-core';
+import type { Metadata } from 'next';
 
 import { fetchMainBanners } from '@/api/domains/main';
 import ViewPost from '@/app/post/view';
 import HydrateOnClient from '@/components/Context/HydrateOnClient';
 import getQueryClient from '@/utils/query/getQueryClient';
+
+export const metadata: Metadata = {
+  title: 'Post',
+};
 
 const Post = async () => {
   const sampleId = 1;
