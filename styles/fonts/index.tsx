52d17b774dc12b7fd53551584e51fd9cbe22586e
Seong Gi Dong <sgd0947@gmail.com>
2023-09-14 16:22:40 +0900

    feat(font): notoSansKr에서 subsets 'korean'미지원으로 인하여 preload false로 변경

--- styles/fonts/index.tsx
+++ styles/fonts/index.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line camelcase
 import { Noto_Sans_KR, Noto_Serif } from 'next/font/google';
-import localFont from 'next/font/local';
+import localFont from 'next/font/local'; // Font files can be colocated inside of `app`
 
 // Font files can be colocated inside of `app`
 export const notoSerifFont = Noto_Serif({
@@ -18,7 +18,7 @@ export const pretendardFont = localFont({
 
 export const notoSansKr = Noto_Sans_KR({
   // preload: true, 기본값
-  subsets: ['latin'], // 또는 preload: false
+  preload: false,
   weight: ['100', '400', '700', '900'], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
   variable: '--font-noto-sans-kr', // 가변 폰트의 경우, 사용할 css 변수명
 });
