8bbbc05c40b4085fc573451c4bc37c1ed4aa2f34
Seong Gi Dong <sgd0947@gmail.com>
2023-09-15 09:24:40 +0900

    fix(home): styles 에러 수정

--- app/home/view.tsx
+++ app/home/view.tsx
@@ -1,15 +1,13 @@
 'use client';
 import styles from './home.module.scss';
 
-import { Container } from '@/app/home/styles';
-
 const ViewHome = () => {
   return (
-    <Container className={styles.container}>
+    <div className={styles.container}>
       <h1>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
-    </Container>
+    </div>
   );
 };
 
